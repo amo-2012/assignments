f66a594b364f39b4186c060752caddaa10f2c615
anno <amokeeffe@ymail.com>
2012-10-28 23:29:26 -0400

    first commit.

---
+++ homework-9.1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * Homework week 9   Oct 27 2012.   Coding Dojo
+ * practice 1
+*/
+
+
+function add ( $a, $b )
+{
+ 
+    return $a + $b;  
+
+}
+
+        
+assert (add (1,0)  == 1);
+
+?>
