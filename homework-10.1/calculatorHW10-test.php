ca834299276f2c62ad5f91fba945574c12d2af04
anno <amokeeffe@ymail.com>
2012-11-04 22:19:57 -0500

    calculator in 2 files

---
+++ homework-10.1/calculatorHW10-test.php
@@ -0,0 +1,18 @@
+<?php
+/*  Homework, week 10  Nov 3 2012
+    This file is to have the assertions that will be used for the tests
+
+*/
+
+
+require 'calculatorHW10.php';
+{
+
+assert (add (1,0)  == 1);
+assert (add (1,1)  == 2);
+assert (add (1,-1) == 0);
+
+echo "testing is true";
+}
+
+?>
---
+++ homework-10.1/calculatorHW10.php
@@ -0,0 +1,20 @@
+<?php
+/* Homework, week 10  Nov 3 2012
+    This file will have the function 
+*/
+function add ( $a, $b )
+    {
+     return $a + $b;         
+    }
+echo '$a + $b';
+
+
+function sub ( $a, $b )
+{
+ 
+    return $a - $b;         
+ 
+}
+echo "true". "calculator is working  YEAH!";
+
+?>
