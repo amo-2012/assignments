f98ee3eb3184030909bcecd1abc6bbcee61c00c6
anno <amokeeffe@ymail.com>
2012-12-12 23:43:15 -0500

    3rd pass and 4th pass.

--- 100-door-Matrix/productionDoors.php
+++ 100-door-Matrix/productionDoors.php
@@ -17,8 +17,9 @@
         $doors [$i] = 'open';
         
      }
-     
+echo 'Pass 1 - doors status: ';     
 print_r(array_values ($doors));
+
 
 // now I will toggle even door from closed to open
 for ($i = 1; $i <=10; $i++)
@@ -30,10 +31,49 @@ for ($i = 1; $i <=10; $i++)
         
     }
 }
-
+echo 'Pass 2 - doors status: ';
 print_r(array_values($doors));
 
 //  I have a a results of the even doors being closed and the odd doors are opened.    
 // SUCCESSFUL  2nd pass !
 
 
+
+// now for my 3rd trick, I toggle every 3rd door   >   3RD PASS
+
+for ($i =1; $i <=10; $i++)
+{
+    if (($i % 3) == 0)
+    {
+        if ($doors [$i] == 'closed')
+        $doors [ $i] = 'open';
+    }
+    else
+    {
+        if ($doors [$i] == 'open')
+        $doors [$i] = 'closed';
+       
+    }
+    
+}
+echo 'Pass 3 - doors status: ';
+print_r (array_values($doors));
+
+
+// for 4th pass, we will toggle every 4th door
+
+for ($i=1; $i <=10; $i++)
+{
+    if (($i %4 ) == 0)
+    {
+        if ($doors [$i] == 'closed')
+        $doors [$i] = 'open';
+    }
+    else
+    {
+        if ($doors [$i] == 'open')
+        $doors [$i] = 'closed';
+    }
+}
+echo 'Pass 4 - doors status: ';
+print_r(array_values($doors));
