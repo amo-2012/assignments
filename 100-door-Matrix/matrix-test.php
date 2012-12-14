c677f4f8d23ae3c661460efdc9bce26698d6e13c
anno <amokeeffe@ymail.com>
2012-12-14 00:26:09 -0500

    more practice.

--- 100-door-Matrix/matrix-test.php
+++ 100-door-Matrix/matrix-test.php
@@ -1,42 +1,27 @@
 <?php
-/** 100 door hallway. At first pass, open all the doors.
- * at 2nd pass, close every other door,
- * at 3rd pass, toggle every 3rd door
- * at 4th pass, toggle every 4th door.
+
+/**
  * 
- * this file will set up the tests.
+ * 
  * 
  */
- require_once "productionDoors.php";
  
- if (is_array ($doors))
-    echo '        The hallway is there';
+require_once "productionDoors.php";
+ 
+if (is_array ($doors))
+    echo '  The hallway of doors does exist. ';
     
     
-if (assert (count ($doors) ==10 ) )
-    echo '   and there are 10 doors in the hallway';
-                  // will add more doors once the pattern is established
-                  
-                  
+if (assert (count ($doors) == 10 ))
+    echo ' there are ten doors here';
+    
+    
 function open_closed ($doors)
+{
+    for ($i = 1; $i <= 10; $i++)
     {
-        for ($i = 1; $i <= 10; $i++)
-        {
-            assert ($doors [$i] == ' open   ');
-            echo $i . 'door' . $doors [$i];
-        }
-    }
- 
-function test_evenDoors ($doors)
-    {
-        for ( $i =1; $i <=10; $i++)
-        {
-            if (($i %2 ) == 0)
-                if (assert ($doors [$i] == 'closed'))
-                echo $i . '  is now  CLOSED   ';                
-              
-        }
-        
+        assert ($doors [$i] == 'open ');
+        echo $i . 'door' $doors [$i];
     }
     
-test_evenDoors ($doors);
+}
--- 100-door-Matrix/productionDoors.php
+++ 100-door-Matrix/productionDoors.php
@@ -1,79 +1,33 @@
 <?php
+
 /**
- * this is the code for the production
- * 
- * 
- */
- 
- echo "this is the production code for the 100 door matrix \n we are going to start with only 10 doors to establish the pattern before filling the array with 100 doors";
- $doors = array();
- $doors = array_fill (1,10,'closed');
- 
- print_r(array_values($doors));
- 
- for ($i = 1; $i <=10; $i++)
- {
-     if ($doors [$i ] == 'closed')
-        $doors [$i] = 'open';
-        
-     }
-echo 'Pass 1 - doors status: ';     
-print_r(array_values ($doors));
+* production code for the matrix
+*/
 
+$doors = array();
+$doors = array_fill(1,10, 'closed');
 
-// now I will toggle even door from closed to open
-for ($i = 1; $i <=10; $i++)
-{
-    if (($i % 2) == 0)
-    {
-        if ($doors [$i] == 'open')
-        $doors [$i ] = 'closed';
-        
-    }
-}
-echo 'Pass 2 - doors status: ';
 print_r(array_values($doors));
-
-//  I have a a results of the even doors being closed and the odd doors are opened.    
-// SUCCESSFUL  2nd pass !
-
-
-
-// now for my 3rd trick, I toggle every 3rd door   >   3RD PASS
 
 for ($i =1; $i <=10; $i++)
 {
-    if (($i % 3) == 0)
-    {
-        if ($doors [$i] == 'closed')
-        $doors [ $i] = 'open';
+    if ($doors [$i] == 'closed');
+    $doors [$i ] = 'open';
     }
-    else
-    {
-        if ($doors [$i] == 'open')
-        $doors [$i] = 'closed';
-       
-    }
-    
-}
-echo 'Pass 3 - doors status: ';
-print_r (array_values($doors));
 
-
-// for 4th pass, we will toggle every 4th door
-
-for ($i=1; $i <=10; $i++)
-{
-    if (($i %4 ) == 0)
-    {
-        if ($doors [$i] == 'closed')
-        $doors [$i] = 'open';
-    }
-    else
-    {
-        if ($doors [$i] == 'open')
-        $doors [$i] = 'closed';
-    }
-}
-echo 'Pass 4 - doors status: ';
 print_r(array_values($doors));
+
+
+for ($i = 1; $i <=10; $i++)
+
+    { 
+        if (($i % 2) == 0)
+        {
+         if ($doors [$i] == 'open');
+        $doors [$i] = 'closed';
+        }
+  
+    }
+ print_r(array_values($doors));   
+    
+
