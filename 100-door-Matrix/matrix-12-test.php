6588092d5e605561ea38a66d6df0c0dbd5e7112a
anno <amokeeffe@ymail.com>
2012-12-14 22:38:35 -0500

    now toggling 7 doors

--- 100-door-Matrix/matrix-12-test.php
+++ 100-door-Matrix/matrix-12-test.php
@@ -6,7 +6,7 @@
  * 
  */
  
-require_once 'productionDoors.php';
+require_once "productionDoors.php";
  
 if (is_array ($doors))
     echo '  The hallway of doors does exist. ';
@@ -21,6 +21,7 @@ function open_closed ($doors)
     for ($i = 1; $i <= 10; $i++)
     {
         assert ($doors [$i] == 'open');
+        echo $i  'door' $doors[$i];
     }
     
 }
--- 100-door-Matrix/productionDoors.php
+++ 100-door-Matrix/productionDoors.php
@@ -4,20 +4,26 @@
 * production code for the matrix
 */
 
+
+// set of hallway of doors
 $doors = array();
 $doors = array_fill(1,10, 'closed');
 
+echo 'hallway of 10 closed doors:';
 print_r(array_values($doors));
 
+
+// 1st pass  - opening all the closed doors
 for ($i =1; $i <=10; $i++)
 {
     if ($doors [$i] == 'closed');
-    $doors [$i ] = 'open';
+    $doors [$i] = 'open';
     }
-
+echo '1ST PASS  -  opening all the closed doors  ';
 print_r(array_values($doors));
 
 
+//  2nd pass - closing / toggle every other door
 for ($i = 1; $i <=10; $i++)
 
     { 
@@ -29,5 +35,110 @@ for ($i = 1; $i <=10; $i++)
   
     }
  print_r(array_values($doors));   
-    
+echo '2ND PASS - toggle every other door - was open.  E/O door is now closed ';
 
+    
+// 3rd pass  - toggle every 3rd door
+
+for ($i=1; $i<=10; $i++)
+{
+    if (( $i % 3) == 0)
+    {
+        if ($doors [$i] == 'open');
+        $doors [$i] ='closed';
+      }
+      
+     else
+        {
+            if ($doors [$i] == 'closed')
+            $doors [$i] = 'open';
+         }
+}
+print_r(array_values($doors));
+echo '3RD PASS - toggle every 3rd door >  if it was closed, now opened; if opened, now its closed     ';
+
+
+
+// 4th pass  - toggle every 4th door
+
+for ($i=1; $i<=10; $i++)
+{
+    if (( $i % 4) == 0)
+    {
+        if ($doors [$i] == 'open');
+        $doors [$i] ='closed';
+      }
+      
+     else
+        {
+            if ($doors [$i] == 'closed')
+            $doors [$i] = 'open';
+         }
+}
+print_r(array_values($doors));
+echo '4th PASS - toggle every 4 door >  if it was closed, now opened; if opened, now it\'s closed';
+
+
+
+// 5th pass  - toggle every 5th door
+
+for ($i=1; $i<=10; $i++)
+{
+    if (( $i % 5) == 0)
+    {
+        if ($doors [$i] == 'open');
+        $doors [$i] ='closed';
+      }
+      
+     else
+        {
+            if ($doors [$i] == 'closed')
+            $doors [$i] = 'open';
+         }
+}
+
+print_r(array_values($doors)); 
+echo ' 5th PASS - toggle every 5th door' ;
+
+
+
+// 6th pass  - toggle every 6th door
+
+for ($i=1; $i<=10; $i++)
+{
+    if (( $i % 6) == 0)
+    {
+        if ($doors [$i] == 'open');
+        $doors [$i] ='closed';
+      }
+      
+     else
+        {
+            if ($doors [$i] == 'closed')
+            $doors [$i] = 'open';
+         }
+}
+
+print_r(array_values($doors)); 
+echo ' 6th PASS - toggle every 6th door' ;
+
+
+// 7th pass  - toggle every 7th door
+
+for ($i=1; $i<=10; $i++)
+{
+    if (( $i % 7) == 0)
+    {
+        if ($doors [$i] == 'open');
+        $doors [$i] ='closed';
+      }
+      
+     else
+        {
+            if ($doors [$i] == 'closed')
+            $doors [$i] = 'open';
+         }
+}
+
+print_r(array_values($doors)); 
+echo ' 7th PASS - toggle every 7th door' ;
